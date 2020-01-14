cdd023246eabb5ea63c5ba86ef73d1e5e72620db
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-14 15:01:17 -0600

    added dashed border with 2 unique event listeners, and extra one unique event for scale.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 // Your code goes here
+
+
+
+// Catch the intro image
 const introImg = document.querySelector("img")
 
-introImg.addEventListener('mouseenter', () => {
-    introImg.style.transform = 'scale(1.2)'
+
+// When hover over the border will appear
+introImg.addEventListener("mouseenter", () => {
+    introImg.setAttribute("style", "border: 2px dashed gray;")
 })
 
-introImg.addEventListener("mouseleave", function() {
+// When not hover over the border will disappear
+introImg.addEventListener("mouseleave", () => {
+    introImg.setAttribute("style", "border: none;")
+})
+
+// When click the scale will change to 1.2
+introImg.addEventListener("click", () => {
+    introImg.style.transform = "scale(1.5)"
+    introImg.style.transition = "transform 1s"
+})
+
+// When double click the scale will change back to default (1.0)
+introImg.addEventListener("dblclick", () => {
     introImg.style.transform = "scale(1)"
 })
