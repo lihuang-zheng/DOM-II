b7a4e35ee0bfca69dcc357d941fbba322eca2c74
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-14 16:03:13 -0600

    completed the 10 unique listeners.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 // Your code goes here
 
+// Catch the body
+const bodyStyle = document.querySelector("body")
+
+
+
 
 // Catch the h1
 const h1Style = document.querySelector("h1")
-
 
 // When hover over, it will transform slowly to scale of 1.5
 h1Style.addEventListener("mouseenter", () => {
@@ -18,23 +22,27 @@ h1Style.addEventListener("mouseleave", () => {
 
 // On click, it will change the background color to light blue.
 h1Style.addEventListener("click", () => {
-    h1Style.style.backgroundColor = "#add8e6"
+    h1Style.style.color = "#add8e6"
 })
 
+
+
+
 // Catch the nav
-const navStyle = document.querySelector("nav")
+const navStyle = document.querySelector("a")
+console.log(navStyle)
 
 // On click, it will change the background color to light blue.
 navStyle.addEventListener("click", (event) => {
-    navStyle.style.backgroundColor = "#add8e6"
+    navStyle.style.color = "#add8e6"
     event.stopPropagation()
 })
 
 
 
+
 // Catch the intro image
 const introImg = document.querySelector("img")
-
 
 // When hover over, the border will appear
 introImg.addEventListener("mouseenter", () => {
@@ -47,14 +55,30 @@ introImg.addEventListener("mouseleave", () => {
 })
 
 // When clicked, the scale will change to 1.2
-introImg.addEventListener("click", () => {
+introImg.addEventListener("click", (event) => {
     introImg.style.transform = "scale(1.5)"
     introImg.style.transition = "transform 1s"
+    bodyStyle.style.webkitFilter = "blur(1px)"
 })
 
 // When double clicked, the scale will change back to default (1.0)
 introImg.addEventListener("dblclick", () => {
     introImg.style.transform = "scale(1)"
+    bodyStyle.style.webkitFilter = "blur(0)"
 })
 
+
+
+
 // Catch h2
+const h2Style = document.querySelector("h2")
+
+// Hover effects for h2
+h2Style.addEventListener("mouseenter", () => {
+    h2Style.style.fontSize = "4rem"
+})
+
+// Un-hover effects for h2
+h2Style.addEventListener("mouseleave", () => {
+    h2Style.style.fontSize = "3.2rem"
+})
