f584d73cd455519f9ac40fd8bd0450d556fa259a
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-14 14:34:14 -0600

    Completed the less compiler, and added js intro image scaling.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const introImg = document.querySelector("img")
+
+introImg.addEventListener('mouseenter', () => {
+    introImg.style.transform = 'scale(1.2)'
+})
+
+introImg.addEventListener("mouseleave", function() {
+    introImg.style.transform = "scale(1)"
+})
